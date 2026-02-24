967bbfa55f998a25f3564cbee92e7564853f050d
Aleeza Zahra <azahradum@gmail.com>
2026-02-25 02:26:35 +0500

    Initial commit of my project

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react' // or vue(), svelte(), etc.
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
     react(),
+    tailwindcss()
  
   ],
 })   
